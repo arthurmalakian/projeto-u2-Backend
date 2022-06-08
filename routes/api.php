5be3d5c33be0eaf59ccdc72fd47d68589865f6a4
Arthur Sérvulo R. dos Santos <assantos@luxfacta.com>
2022-06-07 22:00:03 -0300

    chore: remove needless routes

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,3 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class,'logout']);
     Route::apiResource('devices', DevicesController::class);
 });
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
